1c90cbb3da86dff0236a949fdfde3b539d46159b
Amr Mahmoud <3mrma7mood@gmail.com>
2025-05-15 13:06:35 +0300

    some miner changes

--- frontend/UI/js/renderer.js
+++ frontend/UI/js/renderer.js
@@ -1,51 +1,85 @@
 function updateProgressBar(progressBar, value) {
-  value = Math.round(value);
-  progressBar.querySelector(".progress__fill").style.width = `${value}%`;
-  progressBar.querySelector(".progress_text").textContent = `${value}%`;
+    value = Math.round(value);
+    progressBar.querySelector(".progress__fill").style.width = `${value}%`;
+    progressBar.querySelector(".progress_text").textContent = `${value}%`;
 }
 
-async function getStats() {
-  try {
-    const cpuRes = await fetch('http://127.0.0.1:3000/cpu');
-    const memRes = await fetch('http://127.0.0.1:3000/memory');
-    const procRes = await fetch('http://127.0.0.1:3000/process');
-    const diskRes = await fetch('http://127.0.0.1:3000/disk');
-
-    const cpu = await cpuRes.json();
-    const mem = await memRes.json();
-    const procList = await procRes.json();
-    const disk = await diskRes.json();
-
-    const cpu_usage = document.querySelector(".usage");
-    const memory_usage = document.querySelector(".memory_usage");
-    const disk_usage = document.querySelector(".disk_usage");
-
-    document.getElementById('cpu_name').innerText = `Model: ${cpu.cpu_model}`;
-    document.getElementById('cpu_cores').innerText = `Cores: ${cpu.cpu_cores} `;
-    document.getElementById('cpu_threads').innerText = `Threads: ${cpu.cpu_threads} `;
-    document.getElementById('progress_text').innerText = `${cpu.cpu_percent} %`;
-    document.getElementById('cpu_usage').innerText = `CPU Usage: ${cpu.cpu_percent} %`;
-    document.getElementById('cpu_temp').innerText = `Temperature: ${cpu.cpu_temp} °C`;
-    document.getElementById('cpu_load').innerText = `CPU stats: ${JSON.stringify(cpu.cpu_stats.interrupts)}`;
-    document.getElementById('disk_progress_text').innerText = `${disk.percent} %`;
-    document.getElementById('disk').innerText = `Disk Usage: ${disk.used_gb} GB / ${disk.total_gb} GB`;
-    document.getElementById('memory_progress_text').innerText = `${mem.percent} %`;
-    document.getElementById('memory').innerText = `Memory Usage: ${mem.used_mb} MB / ${mem.total_mb} MB`;
-
-    // Update progress bars
-    updateProgressBar(cpu_usage, cpu.cpu_percent);
-    updateProgressBar(memory_usage, mem.percent);
-    updateProgressBar(disk_usage, disk.percent);
-
-    // Update the process table
-    updateProcessTable(procList);
-
-    document.getElementById('output').style.display = "none";
-  } catch (err) {
-    document.getElementById('output').innerText = "Error connecting to backend.";
-    document.getElementById('output').style.color = "red";
-    console.error(err);
+// Function to start the application and fetch initial data and to run anly one time
+function start() {
+    try {   
+        const cpuRes0 =  fetch('http://127.0.0.1:3000/cpu');
+        const memRes0 =  fetch('http://127.0.0.1:3000/memory');
+        const procRes0 =  fetch('http://127.0.0.1:3000/process');
+        const diskRes0 =  fetch('http://127.0.0.1:3000/disk');
+  
+        const cpu0 =  cpuRes0.json();
+        const mem0 =  memRes0.json();
+        const procList0 =  procRes0.json();
+        const disk0 =  diskRes0.json();
+  
+        const cpu_usage0 = document.querySelector(".usage");
+        const memory_usage0 = document.querySelector(".memory_usage");
+        const disk_usage0 = document.querySelector(".disk_usage");
+  
+        document.getElementById('cpu_name').innerText = `Model: ${cpu0.cpu_model}`;
+        document.getElementById('cpu_cores').innerText = `Cores: ${cpu0.cpu_cores} `;
+        document.getElementById('cpu_threads').innerText = `Threads: ${cpu0.cpu_threads} `;
+        document.getElementById('progress_text').innerText = `${cpu0.cpu_percent} %`;
+        document.getElementById('cpu_usage').innerText = `CPU Usage: ${cpu0.cpu_percent} %`;
+        document.getElementById('cpu_temp').innerText = `Temperature: ${cpu0.cpu_temp} °C`;
+        document.getElementById('cpu_load').innerText = `CPU stats: ${JSON.stringify(cpu0.cpu_stats.interrupts)}`;
+        document.getElementById('disk_progress_text').innerText = `${disk0.percent} %`;
+        document.getElementById('disk').innerText = `Disk Usage: ${disk0.used_gb} GB / ${disk0.total_gb} GB`;
+        document.getElementById('memory_progress_text').innerText = `${mem0.percent} %`;
+        document.getElementById('memory').innerText = `Memory Usage: ${mem0.used_mb} MB / ${mem0.total_mb} MB`;
+  
+        // Update progress bars
+        updateProgressBar(cpu_usage0, cpu.cpu_percent);
+        updateProgressBar(memory_usage0, mem.percent);
+        updateProgressBar(disk_usage0, disk.percent);
+  
+        // Update the process table
+        updateProcessTable(procList0);
+  
+        document.getElementById('output').style.display = "none";
+    } catch (err) {
+        document.getElementById('output').innerText = "Error connecting to backend.";
+        document.getElementById('output').style.color = "red";
+        console.error(err);
+    }
   }
+
+
+// Function to fetch and update system states periodically
+async function getStates() {
+    try {
+        const cpuRes = await fetch('http://127.0.0.1:3000/cpu');
+        const memRes = await fetch('http://127.0.0.1:3000/memory');
+        const diskRes = await fetch('http://127.0.0.1:3000/disk');
+
+        const cpu = await cpuRes.json();
+        const mem = await memRes.json();
+        const disk = await diskRes.json();
+
+        document.getElementById('progress_text').innerText = `${cpu.cpu_percent} %`;
+        document.getElementById('cpu_usage').innerText = `CPU Usage: ${cpu.cpu_percent} %`;
+        document.getElementById('cpu_temp').innerText = `Temperature: ${cpu.cpu_temp} °C`;
+        document.getElementById('cpu_load').innerText = `CPU interrupts: ${JSON.stringify(cpu.cpu_stats.interrupts)}`;
+        document.getElementById('disk_progress_text').innerText = `${disk.percent} %`;
+        document.getElementById('disk').innerText = `Disk Usage: ${disk.used_gb} GB / ${disk.total_gb} GB`;
+        document.getElementById('memory_progress_text').innerText = `${mem.percent} %`;
+        document.getElementById('memory').innerText = `Memory Usage: ${mem.used_mb} MB / ${mem.total_mb} MB`;
+
+        updateProgressBar(cpu_usage, cpu.cpu_percent);
+        updateProgressBar(memory_usage, mem.percent);
+        updateProgressBar(disk_usage, disk.percent);
+
+        document.getElementById('output').style.display = "none";
+    } catch (err) {
+        document.getElementById('output').innerText = "Error connecting to backend.";
+        document.getElementById('output').style.color = "red";
+        console.error(err);
+    }
 }
 
 // Process table functionality
